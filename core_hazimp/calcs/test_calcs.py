9417e7bdb91ba4ac2b6ce64dd1ec1a53b0a0b54a
Duncan Gray <duncan.gray@gmail.com>
2012-11-06 22:25:29 +1100

    pylinting tests

--- core_hazimp/calcs/test_calcs.py
+++ core_hazimp/calcs/test_calcs.py
@@ -1,34 +1,34 @@
-import os
-import sys
+
+"""
+Test the calcs module
+"""
+
 import unittest
 
 from calcs import CALCS
 
 
-class Test_calcs(unittest.TestCase):
-    
-    def setUp(self):
-        pass
-        
-    def tearDown(self):
-        pass
-    
-    def test_Add_a_b_test(self):
+class TestCalcs(unittest.TestCase): 
+    """
+    Test the calcs module
+    """
+
+    def test_AddTest(self):
         inst = CALCS["AddTest"]
-        self.assertEqual(inst(5,20), 25)
+        self.assertEqual(inst(5, 20), 25)
         self.assertEqual(inst.args_in, ['a_test', 'b_test'])
         self.assertEqual(inst.args_out, ('c_test'))
 
         
-    def test_Multiple_values_test(self):
+    def test_MultipleValuesTest(self):
         # Not such a good test though
         inst = CALCS["MultipleValuesTest"]
-        self.assertEqual(inst(5,20), (5, 20))
+        self.assertEqual(inst(5, 20), (5, 20))
         self.assertEqual(inst.args_in, ['a_test', 'b_test'])
         self.assertEqual(inst.args_out, ('e_test', 'f_test'))
         
 #-------------------------------------------------------------
 if __name__ == "__main__":
-    suite = unittest.makeSuite(Test_calcs,'test')
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    Suite = unittest.makeSuite(TestCalcs,'test')
+    Runner = unittest.TextTestRunner()
+    Runner.run(Suite)
