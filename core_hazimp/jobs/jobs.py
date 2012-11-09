1dfb4508f4b743f096ed7006f748e1658b707939
Duncan Gray <duncan.gray@ga.gov.au>
2012-11-09 16:29:15 +1100

    starting to add jobs.  plus file for nose

---
+++ core_hazimp/jobs/jobs.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Jobs are a processing unit that know about the context object.  The
+initial jobs will be for setting up the calculations, such as loading
+the exposure data.
+"""
