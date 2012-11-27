43b8c4f45ce437e7c9bc45780197bd3c202363e3
Duncan Gray <duncan.gray@ga.gov.au>
2012-11-27 17:43:56 +1100

    checking in a bit more. Broken.  Things changing

--- core_hazimp/jobs/jobs.py
+++ core_hazimp/jobs/jobs.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from core_hazimp import csv2dict
 
 """
 Jobs are a processing unit that know about the context object.  The
 initial jobs will be for setting up the calculations, such as loading
 the exposure data.
 """
+
+
+def load_csv_exposure(context):
+    """
+    Read a csv exposure file into the context object.
+    
+    
+    Args:
+       context: The context instance, used to move data around.
+    """
+    
