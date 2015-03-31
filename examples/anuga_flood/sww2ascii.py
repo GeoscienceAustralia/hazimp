6668efd30e9d449cfe1cc600ce07da5cd74b8f1a
dunkgray <duncan.gray@ga.gov.au>
2015-04-01 10:43:27 +1100

    pylinting pep8ing

--- examples/anuga_flood/sww2ascii.py
+++ examples/anuga_flood/sww2ascii.py
@@ -4,8 +4,8 @@ from anuga.file_conversion.sww2dem import sww2dem
 
 sww2dem('big_sydney.sww',
         'big_sydneycs800.asc',
-        quantity = 'stage - elevation',
-        cellsize = 800,
-        number_of_decimal_places = 9,
-        verbose = True,
+        quantity='stage - elevation',
+        cellsize=800,
+        number_of_decimal_places=9,
+        verbose=True,
         block_size=2)
