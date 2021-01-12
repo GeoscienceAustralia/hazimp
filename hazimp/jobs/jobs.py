23a1c28f6039dbc7774816adddd7708261bab99a
Craig Arthur <wcarthur@gmail.com>
2021-01-12 13:06:41 +1100

    Try again to update provenance map output format

--- hazimp/jobs/jobs.py
+++ hazimp/jobs/jobs.py
@@ -816,15 +816,15 @@ class SaveProvenance(Job):
             misc.create_temp_file_path_for_s3(file_name)
         [basename, ext] = os.path.splitext(file_name)
         dot = prov_to_dot(context.prov)
-        dot.write_svg(basename + '.svg')
+        dot.write_png(basename + '.png')
         context.prov.serialize(file_name, format='xml')
         if bucket_key is not None:
             misc.upload_to_s3_if_applicable(file_name,
                                             bucket_name,
                                             bucket_key)
-            misc.upload_to_s3_if_applicable(basename + '.svg',
+            misc.upload_to_s3_if_applicable(basename + '.png',
                                             bucket_name,
-                                            bucket_key[:-len(ext)] + '.svg')
+                                            bucket_key[:-len(ext)] + '.png')
 # ____________________________________________________
 # ----------------------------------------------------
 #                KEEP THIS AT THE END
